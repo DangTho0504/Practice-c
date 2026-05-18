bc31813811554a8c60c26fb5b2402b85cdee025a
Dang Tho <nguyendangtho9@gmail.com>
2026-05-18 21:31:30 +0700

    change

--- bai1/switch-case.cpp
+++ bai1/switch-case.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
-int main(){
-    cout <<"hello world";
+
+int main() {
+
+    int n;
+    int i = 1;
+    int tong = 0;
+
+    cout << "Nhap n: ";
+    cin >> n;
+
+    while(i <= n) {
+
+        tong = tong + i;
+
+        i++;
+
+    }
+
+    cout << "Tong = " << tong;
+
     return 0;
-    
 }

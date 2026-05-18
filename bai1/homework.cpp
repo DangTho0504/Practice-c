7b4c3a18910c60f753e8c44a2abdfcca4d26f8b1
Dang Tho <nguyendangtho9@gmail.com>
2026-05-19 00:19:59 +0700

    thu3

---
+++ bai1/homework.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, gt = 1, tong = 0 ;
+    cout << "Nhap n: ";
+    cin >> n ;
+    // nhập n , nếu n chẵn tính tổng , n lẽ kết thúc
+  if (n % 2 ==0)
+  {
+    for (int i = 0; i <= n; i+=2)
+    {
+        tong += i;
+    }
+    cout << "tong = " << tong << endl;
+  }
+  else {
+    cout << "n le bye" << endl;
+  }
+    // tim số chia hết cho 3 từ 10 đến 50
+for (int k = 10; k < 50; k++)
+{
+    if ( k % 3 == 0)
+    {
+        cout << k << " ";
+    }
+    
+}
+// tính tổng S = 1!+2!+3!+...n!
+int sum = 0;
+
+for (int j = 1; j <= n; j++)
+{
+    gt *= j;
+    sum += gt;
+}
+cout << endl << "S = " << sum << endl;
+//kiem tra số hoàn hảo,
+int add = 0 ;
+
+for (int i = 1; i < n; i++)
+{
+    if (n % i == 0)
+    {
+        add += i;
+    } 
+}
+if (add == n)
+{
+    cout << "so hoan hao";
+} else {
+    cout << " so khong hoan hao";
+}
+
+
+
+
+  return 0 ;
+}

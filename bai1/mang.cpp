51307ed6f076f7bf9cd0f87cb935527b1fc132b2
Dang Tho <nguyendangtho9@gmail.com>
2026-05-22 23:24:19 +0700

    thêm bài mảng

--- bai1/mang.cpp
+++ bai1/mang.cpp
@@ -3,7 +3,34 @@ using namespace std;
 
 int main() {
 
- cout <<"Hello world";
+    int n;
+    int dem = 0;
+
+    cout << "Nhap so phan tu: ";
+    cin >> n;
+
+    int a[100];
+
+    // Nhap mang
+    for(int i = 0; i < n; i++) {
+
+        cout << "Nhap a[" << i << "] = ";
+        cin >> a[i];
+
+    }
+
+    // Dem so chan
+    for(int i = 0; i < n; i++) {
+
+        if(a[i] % 2 == 0) {
+
+            dem++;
+
+        }
+
+    }
+
+    cout << "Co " << dem << " so chan";
 
     return 0;
 }

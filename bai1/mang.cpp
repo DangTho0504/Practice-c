44389b8097015b5be20688f7f51bd6f0e0b7b6b6
Dang Tho <nguyendangtho9@gmail.com>
2026-05-22 23:14:08 +0700

    up từ sourcetree

--- bai1/mang.cpp
+++ bai1/mang.cpp
@@ -3,34 +3,7 @@ using namespace std;
 
 int main() {
 
-    int n;
-    int dem = 0;
-
-    cout << "Nhap so phan tu: ";
-    cin >> n;
-
-    int a[20];
-
-    // Nhap mang
-    for(int i = 0; i < n; i++) {
-
-        cout << "Nhap a[" << i << "] = ";
-        cin >> a[i];
-
-    }
-
-    // Dem so chan
-    for(int i = 0; i < n; i++) {
-
-        if(a[i] % 2 == 0) {
-
-            dem++;
-
-        }
-
-    }
-
-    cout << "Co " << dem << " so chan";
+ cout <<"Hello world";
 
     return 0;
 }

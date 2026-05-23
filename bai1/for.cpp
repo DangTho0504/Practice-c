8427926fd0a6c4d2c0be2225cd1d5fdaf5257168
Dang Tho <nguyendangtho9@gmail.com>
2026-05-23 20:44:02 +0700

    23-5

--- bai1/for.cpp
+++ bai1/for.cpp
@@ -1,11 +1,11 @@
-#include <iostream>>
+#include <iostream>
 using namespace std;
 int main () {
 int n;
 int sum = 0;
  cout << " nhap n: ";
  cin >> n;
- for ( int i = 0 , i < n , i++)
+ for ( int i = 0 ; i < n ; i++)
   {
   sum +=i;
   }

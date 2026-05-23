9ad5867e92eac0471818f091629c0b0a4352405c
DangTho0504 <nguyendangtho9@gmail.com>
2026-05-23 20:40:43 +0700

    Update for.cpp
    them code tren github

--- bai1/for.cpp
+++ bai1/for.cpp
@@ -1,8 +1,14 @@
 #include <iostream>>
 using namespace std;
 int main () {
-
- 
- 
+int n;
+int sum = 0;
+ cout << " nhap n: ";
+ cin >> n;
+ for ( int i = 0 , i < n , i++)
+  {
+  sum +=i;
+  }
+cout << "Sum = " << sum;
     return 0 ;
 }

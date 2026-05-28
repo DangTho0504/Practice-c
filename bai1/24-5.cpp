b935809c2b27a5b428aa2e727fa72b1ad4aff685
Dang Tho <nguyendangtho9@gmail.com>
2026-05-29 00:54:25 +0700

    lam bai tap

--- bai1/24-5.cpp
+++ bai1/24-5.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
 using namespace std;
 int main(){
-int M[6];
-for (int i = 0; i < 6; i++)
+int n;
+cout << "Nhap so phan tu trong mang: ";
+cin >> n;
+int M[100];
+for (int i = 0; i < n; i++)
 {
     cout << "M[" << i << "] = ";
     cin >> M[i];
 }
-// Tăng dần mảng không dùng thư viện
-for (int i = 0; i < 6; i++)
+for (int i = 0; i < n-1; i++)
 {
-    cout << M[i] << " ";
+    for (int j = i + 1; j < n; j++)
+    {
+        if (M[i] > M[j])
+        {
+            int temp = M[i];
+            M[i] = M[j] ;
+            M[j] = temp;
+        }
+        }
+    }
+cout << "Mang sau khi tang dan:\n";
+for (int i = 0; i < n; i++)
+{
+   cout << M[i] << " ";
+
 }
+int a;
+cout << "Nhap so a = ";
+cin >> a;
+for (int i = 0; i < n; i++)
+{
+    if (M[i] == a)
+    {
+        /* code */
+    }
+    
+}
+
 
 return 0;
 }

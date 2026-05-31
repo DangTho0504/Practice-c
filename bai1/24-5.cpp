6ae06e4f208ed8344acb45b21f47148a2e3c99f8
Dang Tho <nguyendangtho9@gmail.com>
2026-05-31 17:14:24 +0700

    homnay

--- bai1/24-5.cpp
+++ bai1/24-5.cpp
@@ -1,94 +1,96 @@
 #include <iostream>
 using namespace std;
-void timMax(int M[], int n){
+void timMax(int M[], int n)
+{
     int max = M[0];
     for (int i = 0; i < n; i++)
     {
         if (M[i] > max)
         {
+
             max = M[i];
         }
     }
-    cout <<"So lon nhat: " << max;
+    cout << "So lon nhat: " << max;
 }
-void xuatMang(int M[], int n){
-for (int i = 0; i < n; i++)
+void xuatMang(int M[], int n)
 {
-   cout << M[i] << " " ;
+    for (int i = 0; i < n; i++)
+    {
+        cout << M[i] << " ";
+    }
 }
-}
-int timMin(int M[], int n){
+int timMin(int M[], int n)
+{
     int min = M[0];
     for (int i = 0; i < n; i++)
     {
         if (M[i] < min)
         {
-            M[i]=min;
+            M[i] = min;
         }
     }
     return min;
-} 
-
-int main(){
-int n;
-cout << "Nhap so phan tu trong mang: ";
-cin >> n;
-int M[100];
-for (int i = 0; i < n; i++)
-{
-    cout << "M[" << i << "] = ";
-    cin >> M[i];
 }
- xuatMang(M,n);
 
-for (int i = 0; i < n-1; i++)
+int main()
 {
-    for (int j = i + 1; j < n; j++)
+    int n;
+    cout << "Nhap so phan tu trong mang: ";
+    cin >> n;
+    int M[100];
+    for (int i = 0; i < n; i++)
     {
-        if (M[i] > M[j])
+        cout << "M[" << i << "] = ";
+        cin >> M[i];
+    }
+    xuatMang(M, n);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
         {
-            int temp = M[i];
-            M[i] = M[j] ;
-            M[j] = temp;
-        }
+            if (M[i] > M[j])
+            {
+                int temp = M[i];
+                M[i] = M[j];
+                M[j] = temp;
+            }
         }
     }
-cout << endl;
-int a; int dem = 0;
-cout << "Nhap so a = ";
-cin >> a;
-bool found = false;
-for (int i = 0; i < n; i++)
-{
-    if (M[i] == a)
+    cout << endl;
+    int a;
+    int dem = 0;
+    cout << "Nhap so a = ";
+    cin >> a;
+    bool found = false;
+    for (int i = 0; i < n; i++)
     {
-       found = true;
-        cout << a << " xuat hien o vi tri " << i << endl;
-        dem++;
-    }  
-}
-if (found)
-{
-      cout << "True" << endl;
-    cout << a << " xuat hien " << dem << " lan\n";
-  
-}
-else
-{
-cout << "False" << endl;
-    cout << a << " khong co trong mang\n";
-    
-}
-cout << "Mang sau khi tang dan:\n";
-for (int i = 0; i < n; i++)
-{
-   cout << M[i] << " ";
-}
-cout << endl;
-timMax(M,n);
-cout << "\nSo nho nhat: " << timMin(M,n);
+        if (M[i] == a)
+        {
+            found = true;
+            cout << a << " xuat hien o vi tri " << i << endl;
+            dem++;
+        }
+    }
+    if (found)
+    {
+        cout << "True" << endl;
+        cout << a << " xuat hien " << dem << " lan\n";
+    }
+    else
+    {
+        cout << "False" << endl;
+        cout << a << " khong co trong mang\n";
+    }
+    cout << "Mang sau khi tang dan:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << M[i] << " ";
+    }
+    cout << endl;
+    timMax(M, n);
+    cout << "\nSo nho nhat: " << timMin(M, n);
 
-
-
-return 0;
+    return 0;
 }
